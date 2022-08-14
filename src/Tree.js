1728233ae12c0d56ee44ce36156a8f4c02fe65a1
haseeb-shoukat <haseebshoukat52@gmail.com>
2022-08-14 17:13:55 +0500

    Write insert and delete functions

--- src/Tree.js
+++ src/Tree.js
@@ -1,7 +1,7 @@
 import { Node } from "./Node";
 
 const Tree = class {
-  constructor(arr) {
+  constructor(arr = []) {
     this.root = this.buildTree([
       ...new Set(
         arr.sort(function (a, b) {
@@ -20,6 +20,44 @@ const Tree = class {
       this.buildTree(arr.slice(middle + 1)),
       this.buildTree(arr.slice(0, middle))
     );
+  }
+
+  insert(value) {
+    this.root = this.#insertRec(value, this.root);
+  }
+
+  #insertRec(value, pos) {
+    if (!pos) return new Node(value);
+    if (value < pos.value) pos.leftNode = this.#insertRec(value, pos.leftNode);
+    else pos.rightNode = this.#insertRec(value, pos.rightNode);
+    return pos;
+  }
+
+  delete(value) {
+    this.root = this.#deleteRec(value, this.root);
+  }
+
+  #deleteRec(value, pos) {
+    if (!pos) return null;
+    if (pos.value === value) {
+      if (!pos.rightNode) {
+        if (!pos.leftNode) return null;
+        return pos.leftNode;
+      }
+      if (!pos.leftNode) return pos.rightNode;
+      let successor = this.#findMin(pos.rightNode);
+      pos.value = successor;
+      pos.rightNode = this.#deleteRec(successor, pos.rightNode);
+      return pos;
+    }
+    if (value < pos.value) pos.leftNode = this.#deleteRec(value, pos.leftNode);
+    else pos.rightNode = this.#deleteRec(value, pos.rightNode);
+    return pos;
+  }
+
+  #findMin(node) {
+    if (!node.leftNode) return node.value;
+    return this.#findMin(node.leftNode);
   }
 };
 
